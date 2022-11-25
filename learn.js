69ef3c0ce97ffb6d6c44fa4fe37a97dfdbf2d446
OkashaTanoli <okashatanoli12345@gmail.com>
2022-11-25 23:01:48 +0500

    initial commit

---
+++ learn.js
@@ -0,0 +1,125 @@
+// console.log("hello world")
+// document.write("hello world")
+// prompt("enter your name")
+// alert("hello world")
+
+
+
+
+// var a; //declaration
+// a = 10; //initialization
+// var a = 12 //declaration and initialization
+
+
+
+// let a;
+// a = 10;
+// let a = 20;
+
+
+// const a = 10;
+// console.log(a)
+
+
+// let a = 10
+// let b = a // b = 10 (value of a is stored in b)
+// a = 12
+// console.log(b)+
+
+
+
+// Primitive Data Types
+// let str = "dawood bhai"
+// let num = 20
+// let isTrue = false
+// let abc = null;
+// let und = undefined;
+
+
+// let a = "anas"
+// let b = "aijaz"
+// // let c = "My name is " + a + b (old way)
+// let c = `My name
+// is ${a} ${b}
+//  abc` //concatenation
+
+// // let d = "My name is  //(It will give you error)
+// //  abc"
+// console.log(c)
+
+// let a = 20
+// console.log(typeof a)
+
+
+
+
+// let a = "abc" // "20"
+// console.log(Number(a)) // NaN
+// console.log(String(true))
+// console.log(Boolean(0))
+
+
+// example
+// let a = prompt("enter number")
+// console.log(typeof Number(a))
+// console.log(Number(a) + 10)
+
+
+
+// Arithmetic operators:
+
+// console.log("Addition", 10 + 20)
+// console.log("Subtraction", 30 - 20)
+// console.log("Multiplication", 30 * 20)
+// console.log("Division", 30 / 20)
+// console.log("Modulus", 10 % 3)
+// console.log("Modulus", 5 % 30)  // Answer will be 5
+
+
+
+// let a = 10
+// a = a + 5
+// a += 5 //Addition Assigning Oper
+// a -= 5 //Subtraction Assigning Oper
+// a *= 5 //Multiplication Assigning Oper
+// a /= 5 //Division Assigning Oper
+// console.log(a)
+
+
+
+
+// let n = 5;
+// console.log(n == 5); // true
+// console.log(n == "5"); // true
+// console.log(n === "5"); // false
+// console.log(n != 5); // false
+// console.log(n > 8); // false
+// console.log(n < 8); // true
+// console.log(n >= 8); // false
+// console.log(n <= 8); // true
+
+
+
+
+
+// let n = 5
+      // 6  + 6
+// let b = ++n + n--
+// let b = n--;
+// ++n //n = n + 1 (PreIncrement)
+// --n //n = n - 1 (PreDecrement)
+// n++;  // n = n + 1  (PostIncrement)
+// n--; // n = n - 1  (PostDecrement)
+// console.log(n)
+// console.log(b)
+
+// console.log(2**3) // Exponentional operator ( 2 * 2 * 2)
+
+
+
+
+//For LOOP
+
+// for(let i = 0; i < 10;i++){
+//     console.log(i)
+// }
